db3ef4c7cfe680964051dc154423035cd71e223d
larsvandriel <lars.van.driel@hotmail.com>
2021-12-27 03:05:07 +0100

    Added Configurations for models InventoryManagementSystem

---
+++ ims_entities/Configurations/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagementSystem.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Entities.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.HasIndex(p => p.Name).IsUnique();
+        }
+    }
+}
