33a9bc9175c0bb740e00955f7d97e6ab14cd9b8d
larsvandriel <lars.van.driel@hotmail.com>
2021-12-27 02:49:03 +0100

    Created Models for InventoryManagementSystem

---
+++ ims_entities/Models/InventoryItemNotificationThreshold.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Entities.Models
+{
+    public class InventoryItemNotificationThreshold
+    {
+        public Guid InventoryId { get; set; }
+        public Inventory Inventory { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public int ThresholdAmount { get; set; }
+        public ThresholdType ThresholdType { get; set; }
+        public string NotificationMethod { get; set; }
+    }
+}
