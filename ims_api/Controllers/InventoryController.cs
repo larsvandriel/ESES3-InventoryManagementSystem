1d630803025a8608b466583a5c92c0a7d6c2b96e
larsvandriel <lars.van.driel@hotmail.com>
2021-12-28 02:19:22 +0100

    Created Controllers InventoryManagementSystem

---
+++ ims_api/Controllers/InventoryController.cs
@@ -0,0 +1,220 @@
+﻿using InventoryManagementSystem.API.Filters;
+using InventoryManagementSystem.Contracts;
+using InventoryManagementSystem.Entities.Extensions;
+using InventoryManagementSystem.Entities.Models;
+using InventoryManagementSystem.Entities.Parameters;
+using InventoryManagementSystem.Entities.ShapedEntities;
+using LoggingService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace InventoryManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IRepositoryWrapper _repository;
+        private readonly LinkGenerator _linkGenerator;
+
+        public InventoryController(ILoggerManager logger, IRepositoryWrapper repository, LinkGenerator linkGenerator)
+        {
+            _logger = logger;
+            _repository = repository;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+        public IActionResult GetInventories([FromQuery] InventoryParameters inventoryParameters)
+        {
+            try
+            {
+                var inventories = _repository.Inventory.GetAllInventories(inventoryParameters);
+
+                var metadata = new
+                {
+                    inventories.TotalCount,
+                    inventories.PageSize,
+                    inventories.CurrentPage,
+                    inventories.TotalPages,
+                    inventories.HasNext,
+                    inventories.HasPrevious
+                };
+
+                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+                _logger.LogInfo($"Returned {inventories.Count} inventories from database.");
+
+                var shapedInventories = inventories.Select(i => i.Entity).ToList();
+
+                var mediaType = (MediaTypeHeaderValue)HttpContext.Items["AcceptHeaderMediaType"];
+
+                if (!mediaType.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return Ok(shapedInventories);
+                }
+
+                for (var index = 0; index < inventories.Count; index++)
+                {
+                    var inventoryLinks = CreateLinksForInventory(((NormalShapedEntity)inventories[index]).Id, inventoryParameters.Fields);
+                    shapedInventories[index].Add("Links", inventoryLinks);
+                }
+
+                var inventoriesWrapper = new LinkCollectionWrapper<Entity>(shapedInventories);
+
+                return Ok(CreateLinksForInventories(inventoriesWrapper));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllOwners action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{id}", Name = "InventoryById")]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+        public IActionResult GetInventoryById(Guid id, [FromQuery] string fields)
+        {
+            try
+            {
+                var inventory = (NormalShapedEntity)_repository.Inventory.GetInventoryById(id, fields);
+
+                if (inventory.Id == Guid.Empty)
+                {
+                    _logger.LogError($"Inventory with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                var mediaType = (MediaTypeHeaderValue)HttpContext.Items["AcceptHeaderMediaType"];
+
+                if (!mediaType.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    _logger.LogInfo($"Returned shaped inventory with id: {id}");
+                    return Ok(inventory.Entity);
+                }
+
+                inventory.Entity.Add("Links", CreateLinksForInventory(inventory.Id, fields));
+
+                return Ok(inventory.Entity);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wring inside GetInventoryById action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateInventory([FromBody] Inventory inventory)
+        {
+            try
+            {
+                if (inventory.IsObjectNull())
+                {
+                    _logger.LogError("Inventory object sent from client is null.");
+                    return BadRequest("Inventory object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Invalid inventory object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+
+                _repository.Inventory.CreateInventory(inventory);
+                _repository.Save();
+
+                return CreatedAtRoute("InventoryById", new { id = inventory.Id }, inventory);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside CreateInventory action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateInventory(Guid id, [FromBody] Inventory inventory)
+        {
+            try
+            {
+                if (inventory.IsObjectNull())
+                {
+                    _logger.LogError("Inventory object sent from client is null.");
+                    return BadRequest("Inventory object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Invalid inventory object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+
+                var dbInventory = _repository.Inventory.GetInventoryById(id);
+                if (dbInventory.IsEmptyObject())
+                {
+                    _logger.LogError($"Inventory with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                _repository.Inventory.UpdateInventory(dbInventory, inventory);
+                _repository.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside UpdateOwner action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteInventory(Guid id)
+        {
+            try
+            {
+                var inventory = _repository.Inventory.GetInventoryById(id);
+                if (inventory.IsEmptyObject())
+                {
+                    _logger.LogError($"Inventory with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                _repository.Inventory.DeleteInventory(inventory);
+                _repository.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside DeleteOwner action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        private IEnumerable<Link> CreateLinksForInventory(Guid id, string fields = "")
+        {
+            var links = new List<Link>
+            {
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(GetInventoryById), values: new {id, fields}), "self", "GET"),
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(DeleteInventory), values: new {id}), "delete_inventory", "DELETE"),
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(UpdateInventory), values: new {id}), "update_inventory", "PUT")
+            };
+
+            return links;
+        }
+
+        private LinkCollectionWrapper<Entity> CreateLinksForInventories(LinkCollectionWrapper<Entity> inventoriesWrapper)
+        {
+            inventoriesWrapper.Links.Add(new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(GetInventories), values: new { }), "self", "GET"));
+
+            return inventoriesWrapper;
+        }
+    }
+}
---
+++ ims_api/Controllers/InventoryItemController.cs
@@ -0,0 +1,226 @@
+﻿using InventoryManagementSystem.API.Filters;
+using InventoryManagementSystem.Contracts;
+using InventoryManagementSystem.Entities.Extensions;
+using InventoryManagementSystem.Entities.Models;
+using InventoryManagementSystem.Entities.Parameters;
+using InventoryManagementSystem.Entities.ShapedEntities;
+using LoggingService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace InventoryManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryItemController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IRepositoryWrapper _repository;
+        private readonly LinkGenerator _linkGenerator;
+
+        public InventoryItemController(ILoggerManager logger, IRepositoryWrapper repository, LinkGenerator linkGenerator)
+        {
+            _logger = logger;
+            _repository = repository;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+        public IActionResult GetAllInventoryItems([FromQuery] InventoryItemParameters inventoryItemParameters)
+        {
+            try
+            {
+
+                var inventoryItems = _repository.InventoryItem.GetAllInventoryItems(inventoryItemParameters);
+
+                var metadata = new
+                {
+                    inventoryItems.TotalCount,
+                    inventoryItems.PageSize,
+                    inventoryItems.CurrentPage,
+                    inventoryItems.TotalPages,
+                    inventoryItems.HasNext,
+                    inventoryItems.HasPrevious
+                };
+
+                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+                _logger.LogInfo($"Returned {inventoryItems.Count} inventory items from database.");
+
+                var shapedInventoryItems = inventoryItems.Select(i => i.Entity).ToList();
+
+                var mediaType = (MediaTypeHeaderValue)HttpContext.Items["AcceptHeaderMediaType"];
+
+                if (!mediaType.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return Ok(shapedInventoryItems);
+                }
+
+                for (var index = 0; index < inventoryItems.Count; index++)
+                {
+                    var supplierLinks = CreateLinksForInventoryItem(((ShapedInventoryItemEntity)inventoryItems[index]).InventoryId, ((ShapedInventoryItemEntity)inventoryItems[index]).ProductId, inventoryItemParameters.Fields);
+                    shapedInventoryItems[index].Add("Links", supplierLinks);
+                }
+
+                var inventoryItemWrapper = new LinkCollectionWrapper<Entity>(shapedInventoryItems);
+
+                return Ok(CreateLinksForInventoryItems(inventoryItemWrapper));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllInventoryItems action: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        [HttpGet("{inventoryId}/{productId}")]
+        [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+        public IActionResult GetInventoryItemByInventoryIdAndProductId(Guid inventoryId, Guid productId, [FromQuery] string fields)
+        {
+            try
+            {
+                var inventoryItem = (ShapedInventoryItemEntity)_repository.InventoryItem.GetInventoryItemByInventoryIdAndProductId(inventoryId, productId, fields);
+
+                if (inventoryItem.InventoryId == Guid.Empty)
+                {
+                    _logger.LogError($"InventoryItem with SupplierId: {inventoryId}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                if (inventoryItem.ProductId == Guid.Empty)
+                {
+                    _logger.LogError($"InventoryItem with SupplierId: {inventoryId} and ProductId: {productId}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                var mediaType = (MediaTypeHeaderValue)HttpContext.Items["AcceptHeaderMediaType"];
+
+                if (!mediaType.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    _logger.LogInfo($"Returned shaped InventoryItem with supplierId: {inventoryId} and productId: {productId}");
+                    return Ok(inventoryItem.Entity);
+                }
+
+                inventoryItem.Entity.Add("Links", CreateLinksForInventoryItem(inventoryItem.InventoryId, inventoryItem.ProductId, fields));
+
+                return Ok(inventoryItem.Entity);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetInventoryItemByInventoryIdAndProductId action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateInventoryItem([FromBody] InventoryItem inventoryItem)
+        {
+            try
+            {
+                if (inventoryItem.IsObjectNull())
+                {
+                    _logger.LogError("InventoryItem object sent from client is null.");
+                    return BadRequest("InventoryItem object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Invalid InventoryItem object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+
+                _repository.InventoryItem.CreateInventoryItem(inventoryItem);
+                _repository.Save();
+
+                return CreatedAtRoute("GetInventoryItemByInventoryIdAndProductId", new { inventoryId = inventoryItem.InventoryId, productId = inventoryItem.ProductId }, inventoryItem);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside CreateInventoryItem action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPut("{inventoryId}/{productId}")]
+        public IActionResult UpdateInventoryItem(Guid inventoryId, Guid productId, [FromBody] InventoryItem inventoryItem)
+        {
+            try
+            {
+                if (inventoryItem.IsObjectNull())
+                {
+                    _logger.LogError("InventoryItem object sent from client is null.");
+                    return BadRequest("InventoryItem object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Invalid InventoryItem object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+
+                var dbInventoryItem = _repository.InventoryItem.GetInventoryItemByInventoryIdAndProductId(inventoryId, productId);
+                if (dbInventoryItem.IsEmptyObject())
+                {
+                    _logger.LogError($"InventoryItem with inventoryId: {inventoryId} and productId: {productId}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                _repository.InventoryItem.UpdateInventoryItem(dbInventoryItem, inventoryItem);
+                _repository.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside UpdateInventoryItem action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpDelete("{inventoryId}/{productId}")]
+        public IActionResult DeleteInventoryItem(Guid inventoryId, Guid productId)
+        {
+            try
+            {
+                var inventoryItem = _repository.InventoryItem.GetInventoryItemByInventoryIdAndProductId(inventoryId, productId);
+                if (inventoryItem.IsEmptyObject())
+                {
+                    _logger.LogError($"InventoryItem with inventoryId: {inventoryId} and productId: {productId}, hasn't been found in db.");
+                    return NotFound();
+                }
+
+                _repository.InventoryItem.DeleteInventoryItem(inventoryItem);
+                _repository.Save();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside DeleteInventoryItem action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        private IEnumerable<Link> CreateLinksForInventoryItem(Guid supplierId, Guid productId, string fields = "")
+        {
+            var links = new List<Link>
+            {
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(GetInventoryItemByInventoryIdAndProductId), values: new {supplierId, productId, fields}), "self", "GET"),
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(DeleteInventoryItem), values: new {supplierId, productId}), "delete_inventoryItem", "DELETE"),
+                new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(UpdateInventoryItem), values: new {supplierId, productId}), "update_inventoryItem", "PUT")
+            };
+
+            return links;
+        }
+
+        private LinkCollectionWrapper<Entity> CreateLinksForInventoryItems(LinkCollectionWrapper<Entity> inventoryItemsWrapper)
+        {
+            inventoryItemsWrapper.Links.Add(new Link(_linkGenerator.GetUriByAction(HttpContext, nameof(GetAllInventoryItems), values: new { }), "self", "GET"));
+
+            return inventoryItemsWrapper;
+        }
+    }
+}
