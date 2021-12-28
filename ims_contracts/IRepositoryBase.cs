2008bbc2e0ceb85913a9c27eb9e7361d9adbe5ab
larsvandriel <lars.van.driel@hotmail.com>
2021-12-28 01:12:42 +0100

    Update IRepositoryBase.cs

--- ims_contracts/IRepositoryBase.cs
+++ ims_contracts/IRepositoryBase.cs
@@ -1,7 +1,13 @@
-﻿namespace ims_contracts
-{
-    public class Class1
-    {
+﻿using System.Linq.Expressions;
 
+namespace InventoryManagementSystem.Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
