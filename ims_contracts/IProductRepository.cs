a9295f697e8c0e4e8d070c348b4a174bba1125dc
larsvandriel <lars.van.driel@hotmail.com>
2021-12-28 01:21:25 +0100

    Added contracts for Repos

---
+++ ims_contracts/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Contracts
+{
+    public interface IProductRepository: IRepositoryBase<Product>
+    {
+    }
+}
