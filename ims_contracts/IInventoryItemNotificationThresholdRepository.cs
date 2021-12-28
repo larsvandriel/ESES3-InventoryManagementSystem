675eafe00a5a9a84e109ff5ee044e053f2466345
larsvandriel <lars.van.driel@hotmail.com>
2021-12-28 01:22:15 +0100

    Added Contracts for Repos

--- ims_contracts/IInventoryItemNotificationThresholdRepository.cs
+++ ims_contracts/IInventoryItemNotificationThresholdRepository.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using InventoryManagementSystem.Entities.Helpers;
+using InventoryManagementSystem.Entities.Models;
+using InventoryManagementSystem.Entities.Parameters;
+using InventoryManagementSystem.Entities.ShapedEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ims_contracts
+namespace InventoryManagementSystem.Contracts
 {
-    internal interface IInventoryItemNotificationThreshold
+    public interface IInventoryItemNotificationThresholdRepository: IRepositoryBase<InventoryItemNotificationThreshold>
     {
+        PagedList<ShapedEntity> GetAllInventoryItemNotificationThresholds(InventoryItemNotificationThresholdParameters inventoryItemNotificationThresholdParameters);
+        ShapedEntity GetInventoryItemNotificationThresholdByInventoryIdProductIdThresholdAmountThresholdTypeAndNotificationMethod(Guid inventoryId, Guid productId, int thresholdAmount, ThresholdType thresholdType, string notificationMethod, string fields);
+        InventoryItemNotificationThreshold GetInventoryItemNotificationThresholdByInventoryIdProductIdThresholdAmountThresholdTypeAndNotificationMethod(Guid inventoryId, Guid productId, int thresholdAmount, ThresholdType thresholdType, string notificationMethod);
+        void CreateInventoryItemNotificationThreshold(InventoryItemNotificationThreshold inventoryItemNotificationThreshold);
+        void DeleteInventoryItemNotificationThreshold(InventoryItemNotificationThreshold inventoryItemNotificationThreshold);
     }
 }
--- ims_contracts/IProductRepository.cs
+++ ims_contracts/IProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using InventoryManagementSystem.Entities.Models;
+using InventoryManagementSystem.Entities.ShapedEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,10 @@ namespace InventoryManagementSystem.Contracts
 {
     public interface IProductRepository: IRepositoryBase<Product>
     {
+        ShapedEntity GetProductById(Guid productId, string fields);
+        Product GetProductById(Guid productId);
+        void CreateProduct(Product product);
+        void UpdateProduct(Product dbProduct, Product product);
+        void DeleteProduct(Product product);
     }
 }
